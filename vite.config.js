877cc7f2b71aa74d073b894f52c988d516a589c6
Bharath261 <76419430+Bharath261@users.noreply.github.com>
2024-12-04 01:35:16 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-  rollupOptions: {
-      external: ['react-icons'],
-    },
-  },
-  build: {
-    rollupOptions: {
-        external: ['framer-motion'],
-      },
-    },
   base: "/react-portfolio/",
 })
