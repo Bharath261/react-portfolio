8951da2881f370a62f0b1c912cc4d77cb82e99de
Bharath Kumar <bharath2612kumar@gmail.com>
2024-12-04 13:29:47 +0530

    Initial commit for react-portfolio

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/vite-react-portfolio/", // Replace with your repository name
+  base: "/react-portfolio/", // Replace with your repository name
 });
